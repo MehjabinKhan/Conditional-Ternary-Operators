e680be3e4b1244e21f8a89a4efa5add94ee8afda
MehjabinKhan <161743992+MehjabinKhan@users.noreply.github.com>
2024-03-20 23:43:11 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+"use strict";
+// Conditional Ternary Operators
+let isStuding = true;
+let maths = isStuding ? "Linear Equation" : "Quadratic Equation";
+console.log(`You should be fresh mind for: ${maths}.`);
+let isGoing = false;
+let shopping = isGoing ? "Clothes" : "Makeup";
+console.log(`I am going to purchase some: ${shopping}.`);
+let areBaking = true;
+let cakes = areBaking ? "Chocolate" : "Vanilla";
+console.log(`We are baking: ${cakes}`);
+let isHungry = false;
+let snack = isHungry ? "Chips" : "Juice";
+console.log(`you should take some: ${snack}`);
